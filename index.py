a4475410a37ea1015bc6343b662359aaffb5d579
TalhaSuna <talhasuna2003@gmail.com>
2025-07-22 16:02:31 +0300

    İlk commit

---
+++ index.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+img = cv2.imread('face.jpg')
+
+face_cascade = cv2.CascadeClassifier("frontal_face.xml")
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=4)
+
+for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+cv2.imshow("Yüz Tespiti", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
